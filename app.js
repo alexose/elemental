0ad1bad62bc7676b2f6d8eaed0f15a02dcae2349
Alexander Ose <alex@alexose.com>
2014-06-11 11:48:50 -0400

    add basic div count

--- app.js
+++ app.js
@@ -3,48 +3,67 @@ var mongodb = require('mongodb'),
 
 var args = process.argv || [];
 
-if (args.length < 3){
+if (args.length < 3 || args.length > 4){
    console.log('Usage: ' + args[0] + ' <url>');
    process.exit();
 }
 
+var url = args[2];
+
 // Attempt to connect to Mongo
 mongodb.MongoClient.connect('mongodb://127.0.0.1:27017/elemental', function(err, db){
     if (err) {
-        throw err;
+        e = new Error('Failed to initialize Mongo');
+        e.details = err;
+        throw e;
+    } else {
+        init(db);
     }
 });
 
-// Fire up Spooky
-var spooky = new Spooky({
-        child: {
-            transport: 'http'
-        },
-        casper: {
-            logLevel: 'debug',
-            verbose: true
-        }
-    }, function (err) {
-        if (err) {
-            e = new Error('Failed to initialize SpookyJS');
-            e.details = err;
-            throw e;
-        }
+function init(db){
 
-        spooky.start(
-            'http://en.wikipedia.org/wiki/Spooky_the_Tuff_Little_Ghost');
-        spooky.then(function () {
-            this.emit('hello', 'Hello, from ' + this.evaluate(function () {
-                return document.title;
-            }));
+    // Fire up Spooky
+    var spooky = new Spooky({
+            child: {
+                transport: 'http'
+            },
+            casper: {
+                logLevel: 'debug',
+                verbose: true,
+                options: {
+                    clientScripts: ['jquery.min.js']
+                }
+            }
+        }, function(err){
+            if (err) {
+                e = new Error('Failed to initialize SpookyJS');
+                e.details = err;
+                throw e;
+            }
+
+            spooky.start(url);
+
+            spooky.then(function(){
+                this.emit('size', this.evaluate(function(){
+                    var elements = $('div');
+                    return elements.size();
+                }));
+            });
+
+            spooky.run();
         });
-        spooky.run();
+
+    spooky.on('error', function(e, stack){
+        console.error(e);
+
+        if (stack) {
+            console.log(stack);
+        }
     });
 
-spooky.on('error', function (e, stack) {
-    console.error(e);
-
-    if (stack) {
-        console.log(stack);
-    }
-});
+    spooky.on('size', function (size) {
+        console.log(size);
+        process.exit();
+    });
+}
